eda4af901dc5ee199483de875a8a64de76025181
Martin Pernollet <mpernollet@octo.com>
2014-05-16 21:53:06 +0100

    up

--- sandbox.R
+++ sandbox.R
@@ -4,21 +4,20 @@ setwd("/Users/martin/Dev/datasciencecoursera-repos/datasciencecoursera-explo-a2"
 NEI <- readRDS("data/summarySCC_PM25.rds")
 SCC <- readRDS("data/Source_Classification_Code.rds")
 
-
+# content summary
 pollutantSet <- unique(NEI$Pollutant) # 1 single pollutent
 cateogrySet <- unique(NEI$SCC) # 5480 categories
 typeSet <- unique(NEI$type) # "POINT"    "NONPOINT" "ON-ROAD"  "NON-ROAD"
 yearSet <- unique(NEI$year) # 1999 2002 2005 2008
 # emissionNa <- sum(is.na(NEI$Emissions)) # 0 missing values
 
-#neiMat <- as.matrix(NEI)
-
+# ---------------------------------------------------------------------
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
 # Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 yearTotalEmissions <- aggregate(NEI$Emissions ~ NEI$year, NEI, sum)
 plot(yearTotalEmissions)
 
-
+# ---------------------------------------------------------------------
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
 # Use the base plotting system to make a plot answering this question.
 select <- (NEI$fips == "24510")
@@ -27,6 +26,7 @@ baltimore <- NEI[select,]
 baltimoreYearTotalEmissions <- aggregate(Emissions ~ year, baltimore, sum)
 plot(baltimoreYearTotalEmissions, type="l")
 
+# ---------------------------------------------------------------------
 # Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
 s <- split(baltimore, typeSet)
 pointSums <- aggregate(s$POINT$Emissions ~ s$POINT$year, s$POINT, sum)
@@ -40,13 +40,13 @@ lines(nonPointSums, col = "cyan")
 lines(onRoadSums, col = "yellow")
 lines(nonRoadSums, col = "blue")
 
-
 # Which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
 # answer : points, nonraad, onroad
 
 # Which have seen increases in emissions from 1999–2008? 
 # answer : nonpoint
 
+# ---------------------------------------------------------------------
 # Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
 coalCombustionIndex <- grep("coal.*combustion", SCC$"Short.Name", ignore.case=T)
 sum(coalCombustionIndex) # 604 categories selected
@@ -60,10 +60,35 @@ coalCombPerYear <- aggregate(Emissions ~ year, usCoalCombustion, sum)
 plot.new()
 plot(coalCombPerYear, type="l")
 
-# answerstarted to decrease from 1999 to 2002 and then started to to increase from 2002 to 2008
+# answer : started to decrease from 1999 to 2002 and then started to to increase from 2002 to 2008
 
+# ---------------------------------------------------------------------
 # How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
+vehicleMotorIndex <- grep("motor.*vehicle", SCC$"Short.Name", ignore.case=T) # reverse pattern returns no entity
+sum(vehicleMotorIndex) # 59833 categories selected
+vehicleMotorSccs <- SCC[vehicleMotorIndex,"SCC"]
 
+baltimoreVehicleMotorIndex <- (NEI$SCC %in% vehicleMotorSccs & NEI$fips == "24510") 
+sum(baltimoreVehicleMotorIndex)
+baltimoreVehicleMotor <- NEI[baltimoreVehicleMotorIndex, ]
+
+baltimoreVehicleMotorPerYear <- aggregate(Emissions ~ year, baltimoreVehicleMotor, sum)
+plot.new()
+plot(baltimoreVehicleMotorPerYear, type="l")
+
+# answer : remained around 10
+
+# ---------------------------------------------------------------------
 # Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources  
 # in Los Angeles County, California (fips == "06037"). 
+laVehicleMotorIndex <- (NEI$SCC %in% vehicleMotorSccs & NEI$fips == "24510") 
+sum(laVehicleMotorIndex)
+laVehicleMotor <- NEI[laVehicleMotorIndex, ]
+
+laVehicleMotorIndexPerYear <- aggregate(Emissions ~ year, laVehicleMotor, sum)
+plot.new()
+plot(laVehicleMotorIndexPerYear, type="l")
+
 # Which city has seen greater changes over time in motor vehicle emissions?
+
+# VERIFY WHY NO CHANGE AND SIMILAR VALUE?
